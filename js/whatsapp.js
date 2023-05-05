62448be0113e995e4404ea7a40bd9f67e6ad3c72
Novus-Tech237 <novustechnologies7@gmail.com>
2023-05-05 19:58:45 +0100

    new file:   .vscode/settings.json 	new file:   css/bootstrap.css 	new file:   css/responsive.css 	new file:   css/style.css 	new file:   css/style.css.map 	new file:   css/style.scss 	new file:   images/Algo2.png 	new file:   images/OOAID.png 	new file:   images/OOP.png 	new file:   images/about-img.png 	new file:   images/body_bg.jpg 	new file:   images/call-o.png 	new file:   images/call.png 	new file:   images/client.jpg 	new file:   images/client_four.jpg 	new file:   images/client_one.jpg 	new file:   images/client_three.jpg 	new file:   images/client_two.jpg 	new file:   images/contact-img.jpg 	new file:   images/envelope-o.png 	new file:   images/envelope.png 	new file:   images/hero-bg.jpg 	new file:   images/location-o.png 	new file:   images/location.png 	new file:   images/logo.png 	new file:   images/menu.png 	new file:   images/next-white.png 	new file:   images/next.png 	new file:   images/offer-img.jpg 	new file:   images/prev-white.png 	new file:   images/prev.png 	new file:   images/quote.png 	new file:   images/s-1.png 	new file:   images/s-2.png 	new file:   images/s-3.png 	new file:   images/s-4.png 	new file:   images/search-icon.png 	new file:   index.html 	new file:   js/bootstrap.js 	new file:   js/jquery-3.4.1.min.js 	new file:   js/whatsapp.js 	new file:   sales.html 	new file:   service.html

---
+++ js/whatsapp.js
@@ -0,0 +1,11 @@
+var links = [
+    "https://wa.me/2348126325226",
+    "https://wa.me/237652033038",
+    "https://wa.me/237694191567"
+];
+
+document.getElementById("random-link").addEventListener("click",function(event){
+    event.preventDefault();
+    var randomIndex = Math.floor(Math.random()*links.length);
+    window.location.href = links[randomIndex];
+});
