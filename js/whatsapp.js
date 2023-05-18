a2f4e1be5a4a1bcf30b18853fa571a1713afe628
Novus-Tech237 <novustechnologies7@gmail.com>
2023-05-18 10:59:39 +0100

    Changes to be committed: 	modified:   about.html 	modified:   images/Java.jpg 	modified:   index.html 	modified:   js/whatsapp.js 	modified:   sales.html 	modified:   service.html

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -4,8 +4,12 @@ var links = [
     "https://wa.me/237694191567"
 ];
 
+document.querySelector(".year").textContent = new Date().getFullYear()
+
 document.getElementById("random-link").addEventListener("click",function(event){
     event.preventDefault();
     var randomIndex = Math.floor(Math.random()*links.length);
     window.location.href = links[randomIndex];
 });
+
+
